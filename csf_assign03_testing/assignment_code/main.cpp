c5dd724196a4f268b4296d5ac6d0d46b3b4a1124
Senuka Abeysinghe <senukaabeysinghe@Senukas-MacBook-Pro.local>
2025-10-24 00:47:32 -0400

    fixed writeback bugs

--- csf_assign03_testing/assignment_code/main.cpp
+++ csf_assign03_testing/assignment_code/main.cpp
@@ -9,6 +9,7 @@ using namespace std;
 // cache's single block
 struct CacheLine {
     bool valid = false; // does line contain valid data
+    bool dirty = false; // dirty block or not
     unsigned int tag = 0; // tag bits so we know which memory block stored
     unsigned long lastUsed = 0; // used for LRU tracking
 };
@@ -81,6 +82,7 @@ int main(int argc, char **argv) {
 
         vector<CacheLine> &set = cache[setIndex];
         bool hit = false;
+        int hitIndex = -1; // to track which line is hit
         int emptyIndex = -1; // here to track first empty slot
         int evictIndex = 0; // if full then this is index of to-be-evicted block
         unsigned long oldestTime = ULONG_MAX; // use for finding most recently used block
@@ -90,9 +92,10 @@ int main(int argc, char **argv) {
             // cache hits
             if (set[i].valid && set[i].tag == tag) { 
                 hit = true;
-                if (useLRU) {
+                /*if (useLRU) {
                     set[i].lastUsed = timeCounter; // update the recency
-                } 
+                } */
+                hitIndex = i;
                 break;
             }
             
@@ -115,34 +118,66 @@ int main(int argc, char **argv) {
             if (hit) {
                 loadHits++;
                 cycles += 1; // cache hit so you add a cycle
+                if (useLRU) {
+                    set[hitIndex].lastUsed = timeCounter;
+                }
             } else {
                 loadMisses++;
-                cycles += 100 * (blockSize / 4); // cache miss so get block from memory
+                cycles += 100 * (blockSize / 4) + 1; // cache miss so get block from memory
                 
                 // take this block just retrieved and insert into cache
                 int target = (emptyIndex != -1) ? emptyIndex : evictIndex;
+
+                if (emptyIndex == -1 && writePolicy == "write-back" && set[evictIndex].dirty) {
+                    cycles += 100 * (blockSize / 4); // write back dirty block
+                }
+
                 set[target].valid = true;
                 set[target].tag = tag;
                 set[target].lastUsed = timeCounter;
+                set[target].dirty = false;
             }
-        } 
-
-        // code to handle the store operation
-        else if (op == "s") {
+        } else if (op == "s") {
             totalStores++;
             if (hit) {
                 storeHits++;
-                cycles += 1; // cache hit so you add a cycle
+                if (writePolicy == "write-through") {
+                    cycles += 1 + 100; // write cache and memory
+                } else {
+                    cycles += 1; // cache hit so you add a cycle
+                    set[hitIndex].dirty = true; // mark dirty on write-back hit
+                }
+                if (useLRU) {
+                    set[hitIndex].lastUsed = timeCounter;
+                }
             } else {
                 storeMisses++;
-                cycles += 100 * (blockSize / 4); // cache miss so get block from memory
-                
-                // before writing you need to bring block into cache 
                 if (isWriteAlloc) {
+                    cycles += 100 * (blockSize / 4); // cache miss so get block from memory
+                    
+                    // take this block just retrieved and insert into cache
                     int target = (emptyIndex != -1) ? emptyIndex : evictIndex;
+
+                    // this is write-back eviction penalty
+                    if (emptyIndex == -1 && writePolicy == "write-back" && set[evictIndex].dirty) {
+                        cycles += 100 * (blockSize / 4);
+                    }
+
                     set[target].valid = true;
                     set[target].tag = tag;
                     set[target].lastUsed = timeCounter;
+
+                    // write depending on policy
+                    if (writePolicy == "write-through") {
+                        cycles += 1 + 100;
+                        set[target].dirty = false;
+                    } else {
+                        cycles += 1;
+                        set[target].dirty = true;
+                    }
+                } else {
+                    // no-write-allocate so write directly to memory
+                    cycles += 100;
                 }
             }
         }
